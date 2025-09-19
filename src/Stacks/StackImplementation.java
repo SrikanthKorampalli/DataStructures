759976ec268c3a224f646cb11027f320d1a72604
Korampalli Srikanth <srikanthkorampalli89@gmail.com>
2025-09-19 14:17:58 +0530

    Update StackImplementation.java

--- src/Stacks/StackImplementation.java
+++ src/Stacks/StackImplementation.java
@@ -2,10 +2,8 @@ package Stacks;
 
 public class StackImplementation {
         public static void main(String[] args) {
-            // Create a stack of size 5
             StackClass stack = new StackClass(5);
 
-            // Push elements into the stack
             System.out.println("Pushing elements:");
             for (int i = 1; i <= 6; i++) {
                 boolean pushed = stack.push(i * 10);
@@ -16,22 +14,15 @@ public class StackImplementation {
                 }
             }
 
-            // Peek at the top element
             System.out.println("\nTop element (peak): " + stack.peak());
-
-            // Pop elements from the stack
             System.out.println("\nPopping elements:");
             while (!stack.isEmpty()) {
                 System.out.println("Popped: " + stack.pop());
             }
-
-            // Try popping from empty stack
             System.out.println("Trying to pop from empty stack: " + stack.pop());
 
-            // Check if stack is empty
             System.out.println("\nIs stack empty? " + stack.isEmpty());
 
-            // Check current size
             System.out.println("Current stack size: " + stack.size());
         }
     }
